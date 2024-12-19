b9a28d47bbd1939e595b02cdc6f5e1c73ec7ec83
mcinga <mcingakuhle@gmail.com>
2024-12-19 18:50:37 +0200

    Fix: Add python_introduction as a normal directory

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,11 @@
+number1=10
+number2=5
+
+add=number1+number2
+print("Addition of 10 and 5 is" + " "+str(add))
+
+sub=number1-number2
+print("Subtraction of 10 and 5 is" +" "+str(sub))
+
+multiply=number1*number2
+print("Multipication of 10 and 5 is" +" "+str(multiply)) 
---
+++ python_introduction/rectangle_area.py
@@ -0,0 +1,7 @@
+length = 10
+width = 5
+
+area = length * width
+
+# Print the calculated area
+print("The area of the rectangle is:" + " "+ str(area))
